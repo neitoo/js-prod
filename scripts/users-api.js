08785d52135d2392fb83d4f7d9483b1e6031ee8c
neitoo <samuraa855@gmail.com>
2024-01-06 16:14:39 +0500

    add api response

--- scripts/users-api.js
+++ scripts/users-api.js
@@ -1,1 +1,43 @@
 "use strict";
+
+const list = document.querySelector('#list');
+
+async function start() {
+    list.innerHTML = `
+        <div class="d-flex justify-content-center">
+            <div class="spinner-border text-primary" role="status">
+                <span class="sr-only"></span>
+            </div>
+        </div>
+    `;
+    try {
+        const resp = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await resp.json()
+
+        setTimeout(() => {
+            render(data)
+        }, 2000)
+    } catch (error) {
+        list.innerHTML = `
+            <div class="d-flex justify-content-center">
+                <div class="alert alert-danger" role="alert">
+                    Ошибка загрузки данных: ${error.name}
+                </div>
+            </div>
+        `;
+    }
+}
+
+function render(users = {}) {
+    const html = users.map(toHTML).join('');
+    console.log(html);
+    list.innerHTML = html
+}
+
+function toHTML(user){
+    return `
+    <li class="list-group-item rounded my-1">${user.name}</li>
+    `
+}
+
+start();
